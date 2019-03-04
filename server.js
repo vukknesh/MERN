9c17d4c757bec22f0b0a0281ddab253a49042713
Vukknesh <leomcn@hotmail.com>
2019-03-04 15:04:25 -0300

    serverside with validation

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const passport = require("passport");
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 
 const app = express();
+
+//body-parser
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const db = require("./config/keys").mongoURI;
 
@@ -19,8 +24,13 @@ mongoose
   })
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello"));
-app.use(bodyParser.json());
+//Passport middleware
+
+app.use(passport.initialize());
+
+// Passport config
+
+require("./config/passport")(passport);
 
 // Use Routes
 
