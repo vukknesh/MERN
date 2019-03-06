c520fbb3215507c907f0052f195a152ce66b287c
Vukknesh <leomcn@hotmail.com>
2019-03-06 14:30:36 -0300

    dashboard state

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,15 @@ import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard";
 import "./App.css";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 //check for token
 if (localStorage.jwtToken) {
@@ -27,7 +30,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     //logout user
     store.dispatch(logoutUser);
-    //clear current profile--- not done
+    //clear current profile
+    store.dispatch(clearCurrentProfile);
     //redirect to login
     window.location.href = "/login";
   }
@@ -44,6 +48,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
